59530fbc7f92f70dcaa1008398812b26dfc58f05
Chibuzor Okwusiuno <cokwusiu@uoguelph.ca>
2024-12-23 20:29:04 -0500

    created proper error logging

---
+++ cmd/web/handlers.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"text/template"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/pages/home.html",
+		"./ui/html/components/nav.html",
+	}
+	tplate, err := template.ParseFiles(files...)
+	if err != nil {
+		app.errorLog.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	err = tplate.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		app.errorLog.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Displaying a specific snippet with ID %d...", id)
+}
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		// w.WriteHeader(405)
+		// w.Write([]byte("Method Not Allowed"))
+		// This does the same as the above two lines as it calls them both behind the scenes
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("Creating a snippet"))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module code-snippet.samokw
+
+go 1.23.4
